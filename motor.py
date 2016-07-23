58153ad1dda9aca36f8b100f05f32abd7b7e2245
Jonathan <jonquybao@yahoo.com>
2016-07-23 16:51:47 -0700

    added run and stop functions

--- motor.py
+++ motor.py
@@ -2,16 +2,18 @@
 
 from ev3dev.auto import OUTPUT_A, OUTPUT_D, Motor 
 import time
+import sys
 
+
+action = sys.argv[1]
 m_a = Motor(OUTPUT_A)
 m_d = Motor(OUTPUT_D)
 
-m_d.run_forever(duty_cycle_sp = 100)
-m_a.run_forever(duty_cycle_sp = 100)
-
-time.sleep(1)
-
-m_a.stop()
-m_d.stop()
+if action == 'run':
+	m_d.run_forever(duty_cycle_sp = 100)
+	m_a.run_forever(duty_cycle_sp = 100)
+else:
+	m_a.stop()
+	m_d.stop()
 
 print "finished"
