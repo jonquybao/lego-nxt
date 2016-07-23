fb4c0a5616e0b0145f3761b1d997360eac87fb58
Jonathan <jonquybao@yahoo.com>
2016-07-23 10:30:34 -0700

    made motors move together

--- motor.py
+++ motor.py
@@ -2,11 +2,12 @@ from ev3dev.auto import OUTPUT_A, OUTPUT_D, Motor
 import time
 
 m_a = Motor(OUTPUT_A)
-m_a.run_forever(duty_cycle_sp = 100)
-time.sleep(1)
-m_a.stop()
-
 m_d = Motor(OUTPUT_D)
+
 m_d.run_forever(duty_cycle_sp = 100)
+m_a.run_forever(duty_cycle_sp = 100)
+
 time.sleep(1)
+
+m_a.stop()
 m_d.stop()
